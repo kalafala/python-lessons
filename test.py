8dc73bfb45bddd7618a75fc7d6ed138bdae95a3b
KERIM KALAFALA <kerimkalafala@KERIMs-MacBook-Pro.local>
2016-04-03 22:13:19 -0400

    Add credit line

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+
+
+# Credit:  https://www.youtube.com/watch?v=41qgdwd3zAg&list=PLexnxRP3h0cpct0qgJwvFvnWTuQOTIqh0&index=1
+#   And further vidoes in this series
+
 print ('hello world')
 
 # list all builtin functions
