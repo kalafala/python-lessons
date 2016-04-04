b8727cb95052681a880f65cb805953d9422f3d9e
KERIM KALAFALA <kerimkalafala@KERIMs-MacBook-Pro.local>
2016-04-03 21:30:38 -0400

    Examples with if/elif/else while and for

--- test.py
+++ test.py
@@ -13,7 +13,9 @@ print(family[0])
 print(family[0:2])
 print(family[-2])
 print(family[-3:]);
-print(family[0::2]);
+print(family[0::2])
+print(family[::2])
+print(family[::-2])
 
 family.append('socks');
 print(family);
@@ -36,3 +38,30 @@ maximum = max(val1, val2, val3);
 
 # Illustration of concatination
 print("The maximum is " + str(maximum));
+
+# Note that indentation is important !
+if val1>0:
+	print ("val1 " + str(val1) + " is greater than zero")
+	if val2>0:
+		print ("val2 " + str(val2) + " is greater than zero")
+	else:
+		print ("val2" + str(val1) + " is less than or equal to zero")
+	print ("end of if")
+elif val1==0:
+	print ("val1 " + str(val1) + " is equal to zero")
+	print ("end of elif")
+else:
+	print ("val1" + str(val1) + " is less than zero")
+	print ("end of else")
+
+print ("end of if-elif-else");
+
+count=1
+while count<5:
+	print(count)
+	count+=1
+
+for num in ages:
+	print num
+
+
